231b2a0828162cba6500f295f558a868a255ed6a
mehedi hasan <mehedihasanibnc@gmail.com>
2021-02-02 15:24:51 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,22 @@
+const first = 0;
+const second = false;
+
+if(first == second){
+    console.log('condition is true')
+}else{
+    console.log('condition is false')
+}
+//****double equal hocce sudu value check korbe kintu type check korbe na tai etar condtion true hobe */
+
+const first = 0;
+const second = false;
+
+if(first === second){
+    console.log('condition is true')
+}else{
+    console.log('condition is false')
+}
+//********triple equal type ebong value check korar karone condition false dekhabe */
+
+
+//etai muloto double triple er parthokko
