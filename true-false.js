15bb85b6700ecd8418e08f523d3348aeff29887d
mehedi hasan <mehedihasanibnc@gmail.com>
2021-02-02 14:43:47 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// Falsy value:
+// false //false hocce falsy value
+// 0  // 0 bade sob songkha truthy value
+// ""  //empty string falsey value
+// undefined // nicher 3ta falsy value
+// null
+// NaN
+
+// Truthy value:
+// 'falsy' //string er modde ja thakbe tai truthy value
+// " " //string er modde space thkale mane string er modde ja thakbe tai truthy
+
+
+let name = -12
+if( name || name == 0){
+    console.log('condition is true')
+}else{
+    console.log('condition is false')
+}
